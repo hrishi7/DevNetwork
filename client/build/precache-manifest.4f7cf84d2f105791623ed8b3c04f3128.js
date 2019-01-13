870b4ea4f11ab6b66ad05e4922107ff4e0f3893c
Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>
2019-01-13 19:28:06 +0530

    Initial Commit of production code

---
+++ client/build/precache-manifest.4f7cf84d2f105791623ed8b3c04f3128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1eb7f87575e11f01bb8",
+    "url": "/static/js/main.d1eb7f87.chunk.js"
+  },
+  {
+    "revision": "a4c42c18f6a8d39dcdba",
+    "url": "/static/js/1.a4c42c18.chunk.js"
+  },
+  {
+    "revision": "d1eb7f87575e11f01bb8",
+    "url": "/static/css/main.95a31579.chunk.css"
+  },
+  {
+    "revision": "6b299ee9a2e118cb2be86afd7138d021",
+    "url": "/index.html"
+  }
+];
